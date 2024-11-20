b9e748bfe65a14beacb65f179d4c34c0ebc3e50e
Nestor Ramirez <nestorramirez123@gmail.com>
2024-11-20 16:23:53 -0400

    fix: next config

--- next.config.js
+++ next.config.js
@@ -4,22 +4,21 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
-
-module.exports = {
   serverRuntimeConfig: {
     
-      COMUNNITY: process.env.COMUNNITY,
-      ZABBIX_USER: process.env.ZABBIX_USER,
-      ZABBIX_PASSWORD: process.env.ZABBIX_PASSWORD,
-      MONGO_DB: process.env.MONGO_DB,
-      SERVER: process.env.SERVER_URL,
-      PORT: process.env.PORT
-    },
-  // Will be available on both server and client
-  publicRuntimeConfig: {
-      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,      
-      NEXT_PUBLIC_PASSWORD: process.env.NEXT_PUBLIC_PASSWORD,
-      NEXT_PUBLIC_USERNAME: process.env.NEXT_PUBLIC_USERNAME
-  }
-}
+    COMUNNITY: process.env.COMUNNITY,
+    ZABBIX_USER: process.env.ZABBIX_USER,
+    ZABBIX_PASSWORD: process.env.ZABBIX_PASSWORD,
+    MONGO_DB: process.env.MONGO_DB,
+    SERVER: process.env.SERVER_URL,
+    PORT: process.env.PORT
+  },
+    publicRuntimeConfig: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,      
+        NEXT_PUBLIC_PASSWORD: process.env.NEXT_PUBLIC_PASSWORD,
+        NEXT_PUBLIC_USERNAME: process.env.NEXT_PUBLIC_USERNAME
+    }
+};
+
+module.exports = nextConfig;
+ 
